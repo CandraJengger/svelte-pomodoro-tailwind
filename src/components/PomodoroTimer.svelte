7858110dcdb1b9622763cb7842d8ab04a37919fc
CandraJengger <cakjengger@gmail.com>
2021-02-26 21:18:24 +0700

    completing a pomodoro and taking a break

--- src/components/PomodoroTimer.svelte
+++ src/components/PomodoroTimer.svelte
@@ -3,11 +3,18 @@
   const secondsToMinutes = (seconds) => Math.floor(seconds / 60);
   const padWithZeroes = (number) => number.toString().padStart(2, '0');
 
+  const LONG_BREAK_S = minutesToSeconds(20);
+  const SHORT_BREAK_S = minutesToSeconds(5);
+  let completedPomodoros = 0;
+
   // length of a pomodoro in seconds
   const POMODORO_S = minutesToSeconds(25);
 
   // time left in the current pomodoro
   let pomodoroTime = POMODORO_S;
+
+  // to stop interval
+  let interval;
 
   function formatTime(timeInSeconds) {
     const minutes = secondsToMinutes(timeInSeconds);
@@ -15,7 +22,40 @@
     return `${padWithZeroes(minutes)}:${padWithZeroes(remainingSeconds)}`;
   }
 
-  function startPomodoro() {}
+  function startPomodoro() {
+    interval = setInterval(() => {
+      if (pomodoroTime === 0) {
+        completePomodoro();
+      }
+      pomodoroTime -= 1;
+    }, 1000);
+  }
+
+  function completePomodoro() {
+    clearInterval(interval);
+    completedPomodoros++;
+    if (completedPomodoros === 4) {
+      rest(LONG_BREAK_S);
+      completedPomodoros = 0;
+    } else {
+      rest(SHORT_BREAK_S);
+    }
+  }
+
+  function rest(time) {
+    pomodoroTime = time;
+    interval = setInterval(() => {
+      if (pomodoroTime === 0) {
+        idle();
+      }
+      pomodoroTime -= 1;
+    }, 1000);
+  }
+
+  function idle() {
+    clearInterval(interval);
+    pomodoroTime = POMODORO_S;
+  }
 </script>
 
 <section>
