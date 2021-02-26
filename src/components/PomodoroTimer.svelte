499a820b9a4dcfb124087c5a00d6d7f1a9b2a104
CandraJengger <cakjengger@gmail.com>
2021-02-26 20:58:49 +0700

    Add function to converting time and formating time

--- src/components/PomodoroTimer.svelte
+++ src/components/PomodoroTimer.svelte
@@ -1,1 +1,26 @@
-<p>Hi, I'am pomodoro timer. Yo!</p>
+<script>
+  const minutesToSeconds = (minutes) => minutes * 60;
+  const secondsToMinutes = (seconds) => Math.floor(seconds / 60);
+  const padWithZeroes = (number) => number.toString().padStart(2, '0');
+
+  // length of a pomodoro in seconds
+  const POMODORO_S = minutesToSeconds(25);
+
+  // time left in the current pomodoro
+  let pomodoroTime = POMODORO_S;
+
+  function formatTime(timeInSeconds) {
+    const minutes = secondsToMinutes(timeInSeconds);
+    const remainingSeconds = timeInSeconds % 60;
+    return `${padWithZeroes(minutes)}:${padWithZeroes(remainingSeconds)}`;
+  }
+
+  function startPomodoro() {}
+</script>
+
+<section>
+  <p>{formatTime(pomodoroTime)}</p>
+  <footer>
+    <button on:click={startPomodoro}>start</button>
+  </footer>
+</section>
