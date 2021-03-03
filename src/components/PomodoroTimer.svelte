67baca8dc4004fe67ba5d0129430afe0a6dd62a6
CandraJengger <cakjengger@gmail.com>
2021-03-03 17:57:41 +0700

    Styling Buttons: remove outline button

--- src/components/PomodoroTimer.svelte
+++ src/components/PomodoroTimer.svelte
@@ -64,19 +64,19 @@
 
 <section class="w-full h-4/5 p-5 text-center grid grid-flow-row grid-rows-6">
   <span
-    class="row-start-1 row-end-3 flex justify-center items-center text-7xl transform translate-y-5 transition-all duration-200 bg-gray-100 px-5 py-5 rounded-md opacity-80 shadow-md hover:-translate-y-2 hover:opacity-90"
+    class="row-start-1 row-end-3 flex justify-center items-center text-7xl transform translate-y-4 transition-all duration-200 bg-gray-100 px-5 py-5 rounded-md opacity-80 shadow-md hover:-translate-y-0 hover:opacity-90"
   >
     {formatTime(pomodoroTime)}
   </span>
   <footer class="row-start-5 flex justify-center items-center flex-col">
     <button
       on:click={startPomodoro}
-      class="rounded-md w-full flex justify-center items-center px-7 py-5 font-semibold text-xl uppercase tracking-wider text-white bg-gray-900 hover:bg-gray-800 mx-3 mb-4 outline-none border-none"
+      class="rounded-md w-full flex justify-center items-center px-7 py-5 font-semibold text-xl uppercase tracking-wider text-white bg-gray-900 hover:bg-gray-800 mx-3 mb-4 focus:outline-none"
       >start</button
     >
     <button
       on:click={cancelPomodoro}
-      class="rounded-md w-full flex justify-center items-center px-7 py-5 font-semibold text-md uppercase tracking-wider text-gray-800 bg-green-600  hover:bg-green-700 mx-3"
+      class="rounded-md w-full flex justify-center items-center px-7 py-5 font-semibold text-md uppercase tracking-wider text-gray-800 bg-green-600  hover:bg-green-700 mx-3 focus:outline-none"
       >cancel</button
     >
   </footer>
