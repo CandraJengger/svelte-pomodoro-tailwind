7210777ce27266edbc9209e9cbd7ea6aaa1206b0
CandraJengger <cakjengger@gmail.com>
2021-03-04 00:20:39 +0700

    Create new component Title.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,11 @@
 <script>
-  let title = 'Pomopomo';
+  import Title from './components/Title.svelte';
   import TaskList from './components/TaskList.svelte';
   import PomodoroTimer from './components/PomodoroTimer.svelte';
 </script>
 
 <main class="bg-green-500 w-full flex justify-center items-center flex-col">
-  <h1
-    class="block w-full py-4 text-center text-xl font-extrabold tracking-widest"
-  >
-    {title}
-  </h1>
+  <Title title="Pomopomo" />
   <PomodoroTimer />
   <TaskList />
 </main>
---
+++ src/components/Title.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let title;
+</script>
+
+<h1
+  class="block w-full py-4 text-center text-xl font-extrabold tracking-widest"
+>
+  {title}
+</h1>
