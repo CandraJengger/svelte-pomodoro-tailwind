762ee47a6e7c5d88491ecbe34aa3c00f9ec5321e
CandraJengger <cakjengger@gmail.com>
2021-03-09 10:08:44 +0700

    Change the default color red and gray in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: [],
-  darkMode: "class",
+  darkMode: 'class',
   theme: {
+    colors: {
+      red: { ...colors.red, 200: '#fabea7', 100: '#fbe2e5' },
+      gray: { ...colors.coolGray, 400: '#9cada4', 500: '#767c77' },
+    },
     extend: {},
   },
   variants: {
