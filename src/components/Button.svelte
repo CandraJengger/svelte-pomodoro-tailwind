be5d99a3efe90776e0b5b9943077373cb9a7899f
CandraJengger <cakjengger@gmail.com>
2021-03-15 09:54:28 +0700

    Layouting

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -8,9 +8,9 @@
   const dispatch = createEventDispatcher();
   const typeBtn = {
     BTN_PRIMARY:
-      'rounded-md flex justify-center items-center px-12 py-5 font-semibold text-md uppercase tracking-wider bg-gray-500 hover:opacity-80 mx-3 transform transition-all duration-200 scale-100 hover:scale-y-110 focus:scale-y-110 focus:outline-none',
+      'rounded-md flex justify-center items-center px-12 py-5 font-semibold text-md uppercase tracking-wider bg-gray-500 hover:opacity-80 mx-3 transform transition-all duration-200 scale-100 hover:scale-y-110 focus:outline-none',
     BTN_SECONDARY:
-      'rounded-md flex justify-center items-center px-12 py-5 font-semibold text-md uppercase tracking-wider bg-gray-400  hover:opacity-80 mx-3 transform transition-all duration-200 scale-100 hover:scale-y-110 focus:scale-y-110 focus:outline-none',
+      'rounded-md flex justify-center items-center px-12 py-5 font-semibold text-md uppercase tracking-wider bg-gray-400  hover:opacity-80 mx-3 transform transition-all duration-200 scale-100 hover:scale-y-110 focus:outline-none',
   };
 
   function handleClick() {
