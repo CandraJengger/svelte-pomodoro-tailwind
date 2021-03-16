878633ec141483c7c7f5e15e8a7045586904c728
CandraJengger <cakjengger@gmail.com>
2021-03-16 20:10:21 +0700

    Changing padding button and remove translate on block time

--- src/components/BlockTime.svelte
+++ src/components/BlockTime.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-  class="row-start-1 row-end-4 flex justify-center flex-col items-center transform translate-y-4 transition-all duration-200 bg-red-200 px-5 pt-4 rounded-md shadow-md hover:-translate-y-0"
+  class="row-start-1 row-end-4 flex justify-center flex-col items-center transform translate-y-4 transition-all duration-200 bg-red-200 px-5 pt-4 rounded-md shadow-md"
 >
   <span class="text-7xl mb-7">
     {time}
