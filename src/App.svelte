892fd05ec5eda674e4159360a863421c2682831e
CandraJengger <cakjengger@gmail.com>
2021-03-16 21:19:04 +0700

    Trigger Modal with FAB

--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,27 @@
   import PomodoroTimer from './components/PomodoroTimer.svelte';
   import Modal from './components/Modal.svelte';
   import FAB from './components/FloatActionButton.svelte';
-  import { text } from 'svelte/internal';
+
+  // state
+  let open = false;
+  let hidden = true;
+
+  function openModal() {
+    open = true;
+    hidden = false;
+  }
+
+  function closeModal() {
+    open = false;
+    hidden = true;
+  }
 </script>
 
 <main class="bg-red-100 px-8 w-full flex justify-center items-center flex-col">
   <Title title="Pomopomo" />
   <PomodoroTimer />
-  <FAB text="+" on:clickButton={() => console.log('from FAB')} />
-  <Modal />
+  <FAB text="+" on:clickButton={openModal} />
+  <Modal {open} {hidden} on:handleClose={closeModal} />
 </main>
 
 <style>
--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,5 +1,17 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  export let block = false;
+  export let hidden = true;
+
+  const dispatch = createEventDispatcher();
+  const handleClose = () => dispatch('handleClose');
+</script>
+
 <div
   class="fixed w-5/6 top-2/4 left-2/4 transform -translate-y-2/4 -translate-x-2/4 z-50 bg-white shadow-lg rounded-md py-5 px-6"
+  class:block
+  class:hidden
 >
   <input
     type="text"
@@ -8,10 +20,21 @@
   />
 
   <div class="flex justify-end">
-    <button class="mx-2 focus:outline-none text-gray-500">Cancel</button>
+    <button class="mx-2 focus:outline-none text-gray-500" on:click={handleClose}
+      >Cancel</button
+    >
     <button
       class="mx-2 focus:outline-none bg-gray-700 hover:bg-gray-800 text-white px-4 py-2 rounded-md"
       >Save</button
     >
   </div>
 </div>
+
+<style>
+  .block {
+    display: block;
+  }
+  .hidden {
+    display: none;
+  }
+</style>
