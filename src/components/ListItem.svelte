8076bdd687755f5e8807d7a3d71db08d314eec96
CandraJengger <cakjengger@gmail.com>
2021-03-17 23:36:39 +0700

    Create ListItem component

---
+++ src/components/ListItem.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let text = '';
+</script>
+
+<li class="flex justify-start">
+  <img src="../check.svg" alt="" />
+  <p class="ml-8">{text}</p>
+</li>
--- src/components/TaskList.svelte
+++ src/components/TaskList.svelte
@@ -1,7 +1,28 @@
 <script>
+  import ListItem from './ListItem.svelte';
+
   import { todos } from '../store';
+  let todos_dummy = [
+    {
+      text: 'Macul',
+      finished: false,
+    },
+    {
+      text: 'Ngarit',
+      finished: false,
+    },
+    {
+      text: 'Mancing',
+      finished: false,
+    },
+  ];
 </script>
 
-<div class="row-start-6 row-end-10 container bg-red-200">
+<div class="row-start-6 row-end-10 p-3 px-4 container bg-red-200">
   <h1>task list</h1>
+  <ul class="flex items-center justify-center flex-col list-none">
+    {#each todos_dummy as { text, finished }, i}
+      <ListItem {text} />
+    {/each}
+  </ul>
 </div>
